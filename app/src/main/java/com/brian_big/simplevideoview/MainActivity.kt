6e881aef69a867f1582ac1f5f3659e936f5e940a
Brian-big <briannbig@gmail.com>
2020-11-09 06:52:38 +0300

    preserve playback position

--- app/src/main/java/com/brian_big/simplevideoview/MainActivity.kt
+++ app/src/main/java/com/brian_big/simplevideoview/MainActivity.kt
@@ -4,17 +4,23 @@ import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.widget.MediaController
 import android.widget.VideoView
 
 class MainActivity : AppCompatActivity() {
     private val VIDEO_SAMPLE: String = "tacoma_narrows"
+    private val PLAYBACK_TIME: String = "play_time"
+    private var currentPosition: Int = 0
     lateinit var videoView: VideoView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         videoView = findViewById(R.id.videoview)
+        if (savedInstanceState != null) {
+            currentPosition = savedInstanceState.getInt(PLAYBACK_TIME)
+        }
         val controller = MediaController(this)
         controller.setMediaPlayer(videoView)
         videoView.setMediaController(controller)
@@ -35,6 +41,11 @@ class MainActivity : AppCompatActivity() {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N)
             videoView.pause()
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(PLAYBACK_TIME, videoView.currentPosition)
+    }
     private fun getMedia(mediaName: String): Uri {
         return Uri.parse("android.resource://" + packageName +
                 "/raw/" + mediaName)
@@ -42,6 +53,9 @@ class MainActivity : AppCompatActivity() {
     private fun initializePlayer(){
         val uri = getMedia(VIDEO_SAMPLE)
         videoView.setVideoURI(uri)
+        if (currentPosition > 0)
+            videoView.seekTo(currentPosition)
+        else videoView.seekTo(1)
         videoView.start()
     }
     private fun releasePlayer(){
